ba7633acc54f039adbd028b245567e0ff91f8e0e
Melody <2042204285@qq.com>
2022-12-25 13:42:45 +0800

    moment

--- 函数/浅拷贝.js
+++ 函数/浅拷贝.js
@@ -1,11 +1,14 @@
-function shallowClone(source) {
-  const target = {};
+function shallowCopy(obj) {
+  // 如果不是对象，直接返回
+  if (typeof obj !== "object" || obj === null) return obj;
 
-  for (const key in source) {
-    if (source.hasOwnProperty(key)) {
-      target[key] = source[key];
-    }
+  // 初始化对象
+  const newObj = Array.isArray(obj) ? [] : {};
+
+  // 遍历obj，一一插入新对象
+  for (let key in obj) {
+    obj.hasOwnProperty(key) && (newObj[key] = obj[key]);
   }
 
-  return target;
+  return newObj;
 }
